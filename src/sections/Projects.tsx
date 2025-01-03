f7350ea1ecedc9e0d94f6bc2670a1178e2361fc9
Sana SAIDI <sanasaidi705@gmail.com>
2025-01-03 13:06:22 +0100

    modify image name

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,6 +1,6 @@
 import portfolioImage from "@/assets/images/portfolio.png";
 import TheHyruleCastle from "@/assets/images/TheHyruleCastle.png";
-import MarioKart from "@/assets/images/mariokart.png";
+import MarioKart from "@/assets/images/Mariokart.png";
 import Image from "next/image";
 import CheckcircleIcon from "@/assets/icons/check-circle.svg";
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
